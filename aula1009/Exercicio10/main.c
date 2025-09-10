5e77eda5a542d490a5f6cc52b833f1d765e174bf
maromo71 <professormoraes@gmail.com>
2025-09-10 20:51:32 -0300

    Aula do dia 10 de setembro

---
+++ aula1009/Exercicio10/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(void) {
+    double temp;
+    double F, K, C;
+    char l;
+    printf("Digite uma temperatura e \n");
+    printf("'C' para celsius\n");
+    printf("'F' para fahrenheit\n");
+    printf("'K' para kelvin\n");
+    scanf("%lf %c", &temp, &l);
+    switch (l) {
+        case 'C':
+            printf("Celsius para Fahr..\n");
+            F = temp * 9 / 5.0 + 32;
+            printf("Fahrenheit: %.1f\n", F);
+            printf("Celsius para Kelvin\n");
+            K = temp + 273.15;
+            printf("Kelvin: %.1f\n", K);
+            break;
+        case 'F':
+            printf("Fahrenheit para Celsius..\n");
+            C = (temp - 32) * 5 / 9.0;
+            printf("Celsius: %.1f\n", C);
+            printf("Fahrenheit para Kelvin\n");
+            K = (temp - 32) * 5 / 9.0 + 273.15;
+            printf("Kelvin: %.1f\n", K);
+            break;
+        case 'K':
+            printf("Kelvin para Fahr..\n");
+            F = (temp - 273.15) * 5 / 9.0 + 32.0;
+            printf("Fahrenheit: %.1f\n", F);
+            printf("Kelvin para Celsius\n");
+            C = temp - 273.15;
+            printf("Kelvin: %.1f\n", C);
+            break;
+        default:
+            printf("Opcao invalida");
+    }
+
+    return 0;
+}
